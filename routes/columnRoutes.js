4de686c0580e79315fb246d317fd1747d91dc2af
AngelJara7 <56275399+AngelJara7@users.noreply.github.com>
2023-09-08 21:55:29 -0500

    Delete routes/columnRoutes.js
    eliminado archivo columnRoutes.js

--- routes/columnRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const router = express.Router();
-import { addColumn } from "../controllers/columnControllers.js";
-import checkAuth from "../middleware/checkAuth.js";
-
-router.route('/').post(checkAuth, addColumn);
-
-export default router;
